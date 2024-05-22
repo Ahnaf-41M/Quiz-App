e8a827b4dda92bc682729b4b2106319ae60aeaba
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2024-05-22 17:53:33 +0600

    Update QuestionRepo.java

--- src/main/java/com/main/repository/QuestionRepo.java
+++ src/main/java/com/main/repository/QuestionRepo.java
@@ -1,11 +1,10 @@
 package com.main.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.main.model.Question;
 import org.springframework.data.jpa.repository.*;
 
 @Repository
-public interface QuestionRepo extends JpaRepository<Question, Integer> {
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
 }
