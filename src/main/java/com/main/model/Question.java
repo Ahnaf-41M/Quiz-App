9d036194843c8627b628ddfa5869fb622affd9ad
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2024-05-22 17:51:46 +0600

    Update Question.java

--- src/main/java/com/main/model/Question.java
+++ src/main/java/com/main/model/Question.java
@@ -6,10 +6,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Component
 @Entity
 @Table(name = "questions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Question {
 
 	@Id
@@ -20,85 +26,5 @@ public class Question {
 	private String optionB;
 	private String optionC;
 	private int ans;
-	private int chose;
-
-	public Question() {
-		super();
-	}
-
-	public Question(int quesId, String title, String optionA, String optionB, String optionC,
-			int ans, int chose) {
-		super();
-		this.quesId = quesId;
-		this.title = title;
-		this.optionA = optionA;
-		this.optionB = optionB;
-		this.optionC = optionC;
-		this.ans = ans;
-		this.chose = chose;
-	}
-
-	public int getQuesId() {
-		return quesId;
-	}
-
-	public void setQuesId(int quesId) {
-		this.quesId = quesId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getOptionA() {
-		return optionA;
-	}
-
-	public void setOptionA(String optionA) {
-		this.optionA = optionA;
-	}
-
-	public String getOptionB() {
-		return optionB;
-	}
-
-	public void setOptionB(String optionB) {
-		this.optionB = optionB;
-	}
-
-	public String getOptionC() {
-		return optionC;
-	}
-
-	public void setOptionC(String optionC) {
-		this.optionC = optionC;
-	}
-
-	public int getAns() {
-		return ans;
-	}
-
-	public void setAns(int ans) {
-		this.ans = ans;
-	}
-
-	public int getChose() {
-		return chose;
-	}
-
-	public void setChose(int choosed) {
-		this.chose = choosed;
-	}
-
-	@Override
-	public String toString() {
-		return "Question [quesId=" + quesId + ", title=" + title + ", optionA=" + optionA
-				+ ", optionB=" + optionB + ", optionC=" + optionC + ", ans=" + ans + ", chose="
-				+ chose + "]";
-	}
-
+	private int chosen;
 }
