f6d6fc82e906b9bd6da6b50c7696262d1a36e58b
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2024-05-22 17:52:18 +0600

    Update QuestionForm.java

--- src/main/java/com/main/model/QuestionForm.java
+++ src/main/java/com/main/model/QuestionForm.java
@@ -2,17 +2,16 @@ package com.main.model;
 
 import java.util.List;
 import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Component
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class QuestionForm {
 
 	private List<Question> questions;
 
-	public List<Question> getQuestions() {
-		return questions;
-	}
-
-	public void setQuestions(List<Question> questions) {
-		this.questions = questions;
-	}
 }
