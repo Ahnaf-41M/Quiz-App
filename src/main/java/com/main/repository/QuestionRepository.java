695e8a0bc72bb36598a9dd689dcdbd61072d5ac4
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2024-05-24 20:47:50 +0600

    Update and rename QuestionRepo.java to QuestionRepository.java

--- src/main/java/com/main/repository/QuestionRepository.java
+++ src/main/java/com/main/repository/QuestionRepository.java
@@ -2,9 +2,10 @@ package com.main.repository;
 
 import org.springframework.stereotype.Repository;
 import com.main.model.Question;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
+    List<Question> findByTopicId(int topicId);
 }
