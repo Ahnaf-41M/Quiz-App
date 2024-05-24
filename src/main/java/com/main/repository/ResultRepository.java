c3e7591b942e6dee517320f1f68c757be93104fa
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2024-05-24 20:48:15 +0600

    Update and rename ResultRepo.java to ResultRepository.java

--- src/main/java/com/main/repository/ResultRepository.java
+++ src/main/java/com/main/repository/ResultRepository.java
@@ -1,11 +1,14 @@
 package com.main.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.main.model.Result;
 
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Integer> {
-    Result findByUserId(String userId);
+    List<Result> findByUserId(String userId);
+
+    boolean existsByUserIdAndTopicId(String userId, int topicId);
 
 }
